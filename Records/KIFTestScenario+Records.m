364fcf39756f07b22d53ac8280c1998303ffe532
Simon Baumgartner <me@sens3.com>
2012-11-20 15:50:53 -0500

    a few more steps for the KIF integration tests

--- Records/KIFTestScenario+Records.m
+++ Records/KIFTestScenario+Records.m
@@ -25,11 +25,15 @@
 +(id)viewDetails
 {
     KIFTestScenario *scenario = [KIFTestScenario scenarioWithDescription:@"Viewing the Record Details"];
-
+    
     [scenario addStep:[KIFTestStep stepToTapRowInTableViewWithAccessibilityLabel:@"Records TableView"
                                                                      atIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]]];
     
     [scenario addStep:[KIFTestStep stepToWaitForViewWithAccessibilityLabel:@"Artist Name: Duke Ellington"]];
+    
+    [scenario addStep:[KIFTestStep stepToTapViewWithAccessibilityLabel:@"Records"]];
+    
+    [scenario addStep:[KIFTestStep stepToWaitForViewWithAccessibilityLabel:@"Records TableView"]];
     
     return scenario;
 }
